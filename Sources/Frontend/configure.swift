ed5d1403f702e109f4b64cd933b3598ff0aec73a
Anton Efimenko <reloni@ya.ru>
2020-11-18 16:48:28 +0300

    Refactor configure

--- Sources/Frontend/configure.swift
+++ Sources/Frontend/configure.swift
@@ -5,15 +5,8 @@ import Leaf
 public func configure(_ app: Application) throws {
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    // print(app.directory.viewsDirectory)
-    // print(app.directory.workingDirectory)
-    // var a = app.leaf.renderer.configuration
-    // print(a.rootDirectory)
-    if !app.environment.isRelease {
-        app.leaf.cache.isEnabled = false
-    }
-    
+    app.leaf.cache.isEnabled = app.environment.isRelease
     app.views.use(.leaf)
-    // register routes
+    
     try routes(app)
 }
