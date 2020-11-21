a9d6e5cdc5326bb8a43fe2399aeec609789efe7f
Anton Efimenko <reloni@ya.ru>
2020-11-21 16:26:42 +0300

    Add multiple leaf sources

--- Sources/Frontend/configure.swift
+++ Sources/Frontend/configure.swift
@@ -1,12 +1,35 @@
 import Vapor
 import Leaf
+import LeafKit
 
 // configures your application
 public func configure(_ app: Application) throws {
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
     app.leaf.cache.isEnabled = app.environment.isRelease
+    
+    app.leaf.sources = try configureLeafSources()
     app.views.use(.leaf)
     
     try routes(app)
+}
+
+func configureLeafSources() throws -> LeafSources {
+    let sources = LeafSources()
+    
+    let defaultSource = NIOLeafFiles(fileio: app.fileio, 
+                                    limits: .default, 
+                                    sandboxDirectory: app.directory.workingDirectory, 
+                                    viewDirectory: app.directory.viewsDirectory, 
+                                    defaultExtension: "leaf")
+    let frontendSource = NIOLeafFiles(fileio: app.fileio, 
+                                    limits: .default, 
+                                    sandboxDirectory: app.directory.workingDirectory, 
+                                    viewDirectory: "\(app.directory.viewsDirectory)/Frontend", 
+                                    defaultExtension: "leaf")
+    
+    try sources.register(using: defaultSource)
+    try sources.register(source: "Frontend", using: frontendSource, searchable: true)
+
+    return sources
 }
