aaee4297594256b5cedee2220b9a880291e9599d
Anton Efimenko <reloni@ya.ru>
2020-12-18 00:30:40 +0300

    Show static servers list with leaf

--- Sources/Frontend/Controllers/MainController.swift
+++ Sources/Frontend/Controllers/MainController.swift
@@ -5,12 +5,19 @@ struct MainController: RouteCollection {
 
     func boot(routes: RoutesBuilder) throws {
         routes.get(use: homeView)
+        routes.get("servers", use: serversView)
     }
     
     func homeView(req: Request) throws -> EventLoopFuture<View> {
         return req.leaf.render("index", [
             "title": "myPage - Home",
             "header": "Page header",
+        ])
+    }
+
+    func serversView(req: Request) throws -> EventLoopFuture<View> {
+        return req.leaf.render("servers", [
+            "title": "Active servers"
         ])
     }
 }
