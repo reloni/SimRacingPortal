2060f85f555301b9c1adc5f6048dd52a5f05dfdd
Anton Efimenko <reloni@ya.ru>
2020-12-18 23:50:32 +0300

    Show servers returned by request

--- Sources/Frontend/Controllers/MainController.swift
+++ Sources/Frontend/Controllers/MainController.swift
@@ -1,6 +1,22 @@
 import Vapor
 import Leaf
 
+protocol ViewContext: Content {
+    var title: String { get }
+}
+
+struct Server: Content {
+    let name: String
+    let version: String
+    let port: Int
+    let track: String
+}
+
+struct ServersViewContext: ViewContext {
+    let title: String
+    let servers: [Server]
+}
+
 struct MainController: RouteCollection {
 
     func boot(routes: RoutesBuilder) throws {
@@ -16,8 +32,15 @@ struct MainController: RouteCollection {
     }
 
     func serversView(req: Request) throws -> EventLoopFuture<View> {
-        return req.leaf.render("servers", [
-            "title": "Active servers"
-        ])
+        let servers: [Server] = [
+            .init(name: "Serv 1", version: "1.6.5", port: 9001, track: "Imola"),
+            .init(name: "Serv 2", version: "1.6.5", port: 9002, track: "Barcelona"),
+            .init(name: "Serv 3", version: "1.6.5", port: 9003, track: "Misano"),
+            .init(name: "Serv 5", version: "1.6.5", port: 9004, track: "Suzuka"),
+            .init(name: "Serv 6", version: "1.6.5", port: 9005, track: "Zandvoort")
+        ]
+        let context = ServersViewContext.init(title: "Server management", servers: servers)
+
+        return req.leaf.render("servers", context)
     }
 }
