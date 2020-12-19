df377fe759b5cf2df68ab185d396ebbcf678488e
Anton Efimenko <reloni@ya.ru>
2020-12-19 23:37:29 +0300

    Add example submit form and parsing

--- Sources/Frontend/Controllers/MainController.swift
+++ Sources/Frontend/Controllers/MainController.swift
@@ -17,10 +17,16 @@ struct ServersViewContext: ViewContext {
     let servers: [Server]
 }
 
+struct TextPostRequest: Decodable {
+    let exampleInputEmail1: String
+    let exampleInputPassword1: String
+}
+
 struct MainController: RouteCollection {
 
     func boot(routes: RoutesBuilder) throws {
         routes.get("servers", use: serversView)
+        routes.post("servers", use: postServersView)
     }
     
     func serversView(req: Request) throws -> EventLoopFuture<View> {
@@ -34,5 +40,22 @@ struct MainController: RouteCollection {
         let context = ServersViewContext.init(title: "Server management", servers: servers)
 
         return req.leaf.render("servers", context)
+    }
+
+    func postServersView(req: Request) throws -> EventLoopFuture<Response> {
+        let body = try req.content.decode(TextPostRequest.self)
+        print(body)
+        return req.eventLoop.future(req.redirect(to: "/servers"))
+
+        // let servers: [Server] = [
+        //     .init(name: "Serv 1", version: "1.6.5", port: 9001, track: "Imola"),
+        //     .init(name: "Serv 2", version: "1.6.5", port: 9002, track: "Barcelona"),
+        //     .init(name: "Serv 3", version: "1.6.5", port: 9003, track: "Misano"),
+        //     .init(name: "Serv 5", version: "1.6.5", port: 9004, track: "Suzuka"),
+        //     .init(name: "Serv 6", version: "1.6.5", port: 9005, track: "Zandvoort")
+        // ]
+        // let context = ServersViewContext.init(title: "Server management", servers: servers)
+
+        // return req.leaf.render("servers", context).encodeResponse(for: req)
     }
 }
